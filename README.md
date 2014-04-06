35991d8aca23e14547f03a44ee6f033f3510ac54
Sebastian Krüger <krueger.oh@googlemail.com>
2014-04-06 21:28:01 +0200

    Include small node.js REST server to collect data from mongodb

--- README.md
+++ README.md
@@ -7,6 +7,30 @@ First app inspired by tutorials and articles around the internet
 
 Based on:
 ---------
-* AngularJs
+* AngularJS
 * jquery
 * twitter bootstrap
+* node.js
+* mongodb
+
+Start the engine:
+-----------------
+
+Make sure node.js is installed on the system.
+Start system console and *cd* to the subdir */app* of the project.
+
+Type the following command to start the build in webserver:
+
+    $ node ../scripts/web-server.js
+
+After that you will get access via **localhost:8000** on your webbrowser of choice.
+
+For the next step you need a mongodb instance hosted by yourself or as a service (for example: https://mongolabs.com)
+
+Copy the file *\app\api\config\mongodbconfig.template.json* to *mongodbconfig.json* and fill out your url to mongodb instance.
+
+Open second system console *cd* to subdir */app* and start the REST server to access the mongo DB Data (don't forget to put data in the db!)
+
+    $ node ./api/server.js
+
+When you get your db data in json format under URL **http://localhost:8080/app/jobs** everthing works fine.
---
+++ app/api/server.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Sebastian on 05.04.14.
+ * Restinterface to read data from mongo db
+ */
+
+var express     = require("express");
+var app         = express();
+var mongoose    = require("mongoose");
+var port        = process.env.PORT || 8080;
+var database    = require("./config/mongodbconfig.json");
+
+// Configuration of Database
+var db = mongoose.connect(database.url);
+
+var Job = mongoose.model("Job", {
+    title: String,
+    category: String,
+    votes: Number,
+    company: {
+        name: String,
+        url: String,
+        email: String
+    }
+});
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+
+// Routes
+app.get('/api/jobs', function (req, res) {
+    Job.find(function(err, jobs) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.json(jobs);
+    });
+});
+
+// Listen
+app.listen(port);
+console.log("App listing on port " + port);
