010dc5644563a22259b60ac5950eb8f5f2625267
Sebastian Krüger <krueger.oh@googlemail.com>
2014-04-26 23:19:25 +0200

    Use clientside routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
-var jobsApp = angular.module('jobsApp', ['ngResource']);
+var jobsApp = angular.module('jobsApp', ['ngResource', 'ngRoute'])
+        .config(function ($routeProvider, $locationProvider) {
+            $routeProvider.when('/newJob', {
+                templateUrl: '/templates/NewJob.html',
+                controller: 'NewJobController'
+            })
+                .when('editJob/:jobId', {
+                    templateUrl: '/templates/NewJob.html',
+                    controller: 'NewJobController'
+                })
+                .when('/', {
+                    templateUrl: '/templates/Jobs.html',
+                    controller: 'JobController'
+                })
+                .otherwise({redirectTo: '/'});
+
+        $locationProvider.html5Mode(true);
+    });
